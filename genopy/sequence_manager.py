657494ff9ea649f654c14c2d97dfa90b36ade370
nidhog <ismail.elouafiq@telecom-bretagne.eu>
2016-09-07 13:40:40 +0200

    added Open Reading Frame detection

--- genopy/sequence_manager.py
+++ genopy/sequence_manager.py
@@ -2,7 +2,10 @@
 import settings
 from Bio import SeqIO
 
-class SequenceManager(object):
+class RecordManager(object):
+    """Handles a Record
+
+    """
     def __init__(self, record_file=settings.record_file, record_file_type=settings.default_record_file_type,
                  records=None):
         self.record_file = record_file
@@ -65,14 +68,62 @@ class SequenceManager(object):
         return min_len, len(recs), recs
 
 
-sm = SequenceManager()
-print sm.count_records_b()
-print sm.count_records()
-first = sm.records[0]
-sm.print_all_seq_lengths()
+class SequenceManager(object):
+    """Handles Sequences and Seq Operations
+
+    """
+    def __init__(self, record):
+        self.id = record.id
+        self.seq = record.seq
+        self.record = record
+
+    def __str__(self):
+        s = "----------SEQ---------"+'\n'
+        s += "ID : "+str(self.id)+'\n'
+        s += "SEQ: "+str(self.seq)+'\n'
+        s += "----------------------"+'\n'
+        return s
+
+    def get_orf_list(self, reading_sequence = None):
+        """Returns list of Open Reading Frames
+        An ORF is the part of a Reading Frame
+        that has the potential to encode a protein
+        or a peptide. It is a continuous stretch
+        of codons that do not contain a stop codon
+
+        :param reading_sequence:
+        :return orf_list:
+        """
+        if reading_sequence is None:
+            reading_sequence = self.seq
+        # Three char long codon list
+        codon_list = [reading_sequence[i:i+3] for i in range(0, len(reading_sequence), 3)]
+        orf_list = []
+        i = 0
+        while i < len(codon_list):
+            codon = codon_list[i]
+            if settings.is_start_codon(codon):
+                current_stretch = [codon]
+                while not(settings.is_end_codon(codon)) and i < len(codon_list):
+                    i += 1
+                    codon = codon_list[i]
+                    current_stretch.append(codon)
+                orf_list.append(current_stretch)
+            i += 1
+        return orf_list
+        
+
+rec_m = RecordManager()
+print rec_m.count_records_b()
+print rec_m.count_records()
+first = rec_m.records[0]
+rec_m.print_all_seq_lengths()
 print "---- ----- ----"
-print sm.get_longest_sequences()
+print rec_m.get_longest_sequences()
 print "---- ----- ----"
-print sm.get_smallest_sequences()
+print rec_m.get_smallest_sequences()
+seq_m = SequenceManager(first)
+print seq_m
+print seq_m.get_orf_list()
 
 
--- genopy/settings.py
+++ genopy/settings.py
@@ -1,2 +1,11 @@
 record_file = "input/dna.example.fasta"
 default_record_file_type = "fasta"
+start_codons = ['ATG']
+end_codons = ['TAA', 'TAG', 'TGA']
+
+
+def is_start_codon(codon):
+    return codon in start_codons
+
+def is_end_codon(codon):
+    return codon in end_codons
